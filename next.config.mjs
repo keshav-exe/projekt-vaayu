a93c31c26c02836f5944e91593641d9739cd59c8
Keshav Bagaade <keshav.bagaade@gmail.com>
2024-04-27 22:18:59 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ["files.edgestore.dev", "utfs.io", "ap-south-1.graphassets.com"],
   },
+  // !! WARN !!
+  // Dangerously allow production builds to successfully complete even if
+  // your project has type errors.
+  // !! WARN !!
+  ignoreBuildErrors: true,
 };
 
 export default nextConfig;
