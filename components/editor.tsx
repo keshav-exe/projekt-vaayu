c25a89efef2a4f57489129b582ac4bbd3327a0d6
Keshav Bagaade <keshav.bagaade@gmail.com>
2024-04-28 09:03:06 +0530

    commit

--- components/editor.tsx
+++ components/editor.tsx
@@ -4,7 +4,6 @@ import { useTheme } from "next-themes";
 import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
 import { BlockNoteView, useBlockNote } from "@blocknote/react";
 import "@blocknote/core/style.css";
-import "@blocknote/react/style.css";
 
 import { useEdgeStore } from "@/lib/edgestore";
 
