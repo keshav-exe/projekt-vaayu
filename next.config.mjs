85d0a0c038e5bf8806db2674c3a69fd796f9c4a6
Keshav Bagaade <keshav.bagaade@gmail.com>
2024-04-28 10:22:28 +0530

    PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-  cacheOnFrontEndNav: true,
-  aggresiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  swcMinify: true,
-  disable: false,
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-});
-
-const nextConfig = withPWA({
+const nextConfig = {
   images: {
     domains: ["files.edgestore.dev", "utfs.io", "ap-south-1.graphassets.com"],
   },
-});
+};
 
 export default nextConfig;
